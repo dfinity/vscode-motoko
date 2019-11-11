a7816775ee1ec532158f31b46a0cb66b45cfe2a7
David Millar-Durrant <dmillardurrant@gmail.com>
2019-11-11 14:57:27 +0100

    Working vsix rule

--- default.nix
+++ default.nix
@@ -1,15 +1,37 @@
-{ pkgs ? import <nixpkgs> {}
-, system ? builtins.currentSystem }:
+{ system ? builtins.currentSystem
+, pkgs ? let
+  commonSrc = builtins.fetchGit {
+    url = "ssh://git@github.com/dfinity-lab/common";
+    rev = "90ebf67c410745b113a4ea2308b2c94fda2f0787";
+  }; in
+  import commonSrc {
+    inherit system;
+  }
+}:
 let
   nodePackages = import ./package.nix {
-    inherit pkgs system;
+    inherit system pkgs;
   };
 
-in
-  nodePackages // {
+  withVsix = nodePackages // {
     package = nodePackages.package.override {
       postInstall = "vsce package";
     };
-  }
+  };
 
+  name = "vscode-motoko";
 
+in {
+  vsix = pkgs.stdenv.mkDerivation {
+    inherit name;
+
+    src = ./.;
+
+    buildInputs = [withVsix.package];
+
+    installPhase = ''
+      mkdir -p $out
+      cp vscode-motoko-0.0.1.vsix $out/${name}.vsix
+    '';
+  };
+}
