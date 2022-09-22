890344ad99972b92906e84d2b8848f286eeb7292
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-09-21 23:01:03 -0600

    Update base library before packaging extension

--- utils/downloadBaseLibrary.js
+++ utils/downloadBaseLibrary.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const mo = require('motoko');
 
 (async () => {
-    const package = await mo.fetchPackage('dfinity/motoko-base/master/src');
-    if (!package.name || !Object.entries(package.files).length) {
+    const basePackage = await mo.fetchPackage('dfinity/motoko-base/master/src');
+    if (!basePackage.name || !Object.entries(basePackage.files).length) {
         throw new Error('Unexpected package format');
     }
     fs.writeFileSync(
         __dirname + '/../src/generated/baseLibrary.json',
-        JSON.stringify(package),
+        JSON.stringify(basePackage),
     );
 })().catch((err) => {
     console.error(err);
