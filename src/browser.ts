050fc91a3d761bd7d056e02b6d8dd7233c3cb434
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-10-10 16:34:28 -0600

    Add basic web extension support (#67)

---
+++ src/browser.ts
@@ -0,0 +1,25 @@
+import { ExtensionContext } from 'vscode';
+// import { formatDocument } from './formatter';
+
+// const config = workspace.getConfiguration('motoko');
+
+export function activate(_context: ExtensionContext) {
+    // context.subscriptions.push(
+    //     commands.registerCommand('motoko.startService', () =>
+    //         startServer(context),
+    //     ),
+    // );
+    // context.subscriptions.push(
+    //     languages.registerDocumentFormattingEditProvider('motoko', {
+    //         provideDocumentFormattingEdits(
+    //             document: TextDocument,
+    //             options: FormattingOptions,
+    //         ): TextEdit[] {
+    //             return formatDocument(document, context, options);
+    //         },
+    //     }),
+    // );
+    // startServer(context);
+}
+
+export async function deactivate() {}
