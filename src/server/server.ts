6b76c196689247d9c51c7b5deb93517a92d7db2f
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-10-13 20:19:40 -0600

    Show formatted types on hover (#73)
    * Format hover types using 'prettier-plugin-motoko'
    
    * Update node-motoko
    
    * Misc
    
    * v0.5.0

--- src/server/server.ts
+++ src/server/server.ts
@@ -35,7 +35,12 @@ import DfxResolver from './dfx';
 import ImportResolver from './imports';
 import { getAstInformation } from './information';
 import { findNodes, Program } from './syntax';
-import { getFileText, resolveFilePath, resolveVirtualPath } from './utils';
+import {
+    formatMotoko,
+    getFileText,
+    resolveFilePath,
+    resolveVirtualPath,
+} from './utils';
 
 interface Settings {
     motoko: MotokoSettings;
@@ -725,7 +730,7 @@ connection.onCompletion((event) => {
     return list;
 });
 
-// const ignoredAstNodes = ['AsyncE', 'LetD'];
+// const ignoredAstNodes = [];
 connection.onHover((event) => {
     const { position } = event;
     const { uri } = event.textDocument;
@@ -786,7 +791,7 @@ connection.onHover((event) => {
         isSameLine ? startLine.substring(node.start[1], node.end[1]) : startLine
     ).trim();
     if (node.type) {
-        docs.push(codeSnippet(node.type));
+        docs.push(codeSnippet(formatMotoko(node.type)));
     } else if (!isSameLine) {
         docs.push(codeSnippet(source));
     }
