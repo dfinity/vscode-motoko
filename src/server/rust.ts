7afe1829e883b4177ff2634c9dde3191da60b5ad
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-12-07 14:16:29 -0700

    Improve error handling for Rust functionality

--- src/server/rust.ts
+++ src/server/rust.ts
@@ -4,9 +4,18 @@ const path = __filename.endsWith('rust.ts')
     ? '../../rust/dist/index.node'
     : './vscode_motoko.node';
 
-const rust = require(path);
+let rust: any;
+try {
+    rust = require(path);
+}
+catch(err) {
+    console.error(err);
+}
 
 export function vesselSources(directory?: string | undefined) {
+    if(!rust) {
+        throw new Error('Couldn\'t find a Vessel installation on your system path');
+    }
     const message = rust.vesselSources(resolve(String(directory || '')));
     return message;
 }
