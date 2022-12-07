699092eb20a4dac1a5603b9f01f7f362b8858ca4
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-12-07 14:27:43 -0700

    Visually distinguish context errors

--- src/server/server.ts
+++ src/server/server.ts
@@ -585,10 +585,10 @@ function checkImmediate(uri: string | TextDocument): boolean {
             diagnostics.push({
                 source: virtualPath,
                 message: error,
-                severity: DiagnosticSeverity.Error,
+                severity: DiagnosticSeverity.Information,
                 range: {
                     start: { line: 0, character: 0 },
-                    end: { line: 0, character: 0 },
+                    end: { line: 0, character: 100 },
                 },
             });
         }
