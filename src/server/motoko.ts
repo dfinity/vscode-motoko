81553012d7368e28818e1bc1ea49d68e9e771483
rvanasa <ryan.vandersmith@dfinity.org>
2024-07-16 09:11:44 -0600

    Use built-in 'moc.js'

--- src/server/motoko.ts
+++ src/server/motoko.ts
@@ -1,8 +1,5 @@
-import getMotoko from 'motoko/lib';
-// import mo from 'motoko';
+import mo from 'motoko';
 
 process.env.MOC_UNLOCK_VERIFICATION = '1';
-
-const mo = getMotoko(require('../generated/moc.js').Motoko as any);
 
 export default mo;
