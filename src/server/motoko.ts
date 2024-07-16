bdff671ca4579209fb78dd4f4361dab8188a2f94
rvanasa <ryan.vandersmith@dfinity.org>
2024-07-16 09:15:01 -0600

    Reduce diff

--- src/server/motoko.ts
+++ src/server/motoko.ts
@@ -1,5 +1,8 @@
+// import getMotoko from 'motoko/lib';
 import mo from 'motoko';
 
 process.env.MOC_UNLOCK_VERIFICATION = '1';
+
+// const mo = getMotoko(require('../generated/moc.js').Motoko as any);
 
 export default mo;
