64f3e1f023a22bc5126b31b30f64823000e52e2f
Savely Krendelhoff <alicecandyom@proton.me>
2025-02-20 14:43:04 +0700

    [VSC-6] Add benchmark setup

---
+++ src/server/bench/helpers.ts
@@ -0,0 +1,309 @@
+import assert = require('node:assert');
+import { resolve } from 'node:path';
+import { Duplex } from 'node:stream';
+import {
+    Connection,
+    createConnection,
+    InitializeParams,
+    ProposedFeatures,
+} from 'vscode-languageserver/node';
+import { URI } from 'vscode-uri';
+import { addHandlers } from '../handlers';
+import { forwardMessage } from '../utils';
+const chalk = require('chalk');
+const minimist = require('minimist');
+
+export class MockDuplex extends Duplex {
+    _write(chunk: Buffer, _encoding: BufferEncoding, callback: Function) {
+        this.emit('data', chunk);
+        callback();
+    }
+
+    _read(_size: number) {}
+}
+
+export const configParams = {
+    settings: {
+        motoko: {
+            dfx: 'dfx',
+            legacyLanguageServer: true,
+            maxNumberOfProblems: 100,
+            hideWarningRegex: '',
+            trace: {
+                server: 'verbose',
+            },
+            debugHover: false,
+            canister: '',
+            formatter: 'prettier',
+        },
+    },
+};
+
+export const clientInitParams = (rootUri: URI): InitializeParams => {
+    return {
+        processId: null, // to prevent watchdog killing the process
+        rootPath: rootUri.fsPath,
+        rootUri: rootUri.toString(),
+        workspaceFolders: [
+            {
+                name: 'fixture',
+                uri: rootUri.toString(),
+            },
+        ],
+        capabilities: {
+            textDocument: {
+                synchronization: {
+                    dynamicRegistration: true,
+                    willSave: true,
+                    didSave: true,
+                    willSaveWaitUntil: true,
+                },
+                hover: {
+                    dynamicRegistration: true,
+                    contentFormat: ['markdown', 'plaintext'],
+                },
+                completion: {
+                    dynamicRegistration: true,
+                    completionItem: { snippetSupport: true },
+                },
+                signatureHelp: { dynamicRegistration: true },
+                references: { dynamicRegistration: true },
+                documentHighlight: { dynamicRegistration: true },
+                documentSymbol: { dynamicRegistration: true },
+            },
+            workspace: {
+                applyEdit: true,
+                workspaceEdit: { documentChanges: true },
+                workspaceFolders: true,
+            },
+        },
+    };
+};
+
+export const setupClientServer = (): [Connection, Connection] => {
+    const up = new MockDuplex();
+    const down = new MockDuplex();
+
+    const client = createConnection(down, up);
+
+    client.console.log = forwardMessage(console.log);
+    client.console.warn = forwardMessage(console.warn);
+    client.console.error = forwardMessage(console.error);
+
+    const server = createConnection(ProposedFeatures.all, up, down);
+
+    server.console.log = forwardMessage(console.log);
+    server.console.warn = forwardMessage(console.warn);
+    server.console.error = forwardMessage(console.error);
+
+    client.onRequest('client/registerCapability', () => {});
+    client.listen();
+
+    addHandlers(server, false);
+    server.listen();
+
+    return [client, server];
+};
+
+export const wait = (s: number) =>
+    new Promise((resolve) => setTimeout(resolve, s * 1000));
+
+export async function measureRequest<T>(
+    client: Connection,
+    method: string,
+    params: object,
+): Promise<number> {
+    const start = performance.now();
+    await client.sendRequest<T>(method, params);
+    return performance.now() - start;
+}
+
+export async function measureMultipleRequestConcurrent<T>(
+    client: Connection,
+    method: string,
+    params: object,
+    times: number,
+): Promise<number[]> {
+    assert(times > 1, 'times must be greater than 0');
+    const benchmarks = [...Array(times)].map((_, _i) =>
+        measureRequest<T>(client, method, params),
+    );
+    return await Promise.all(benchmarks);
+}
+
+export async function measureMultipleRequestSequential<T>(
+    client: Connection,
+    method: string,
+    params: object,
+    times: number,
+): Promise<number[]> {
+    assert(times > 1, 'times must be greater than 0');
+    const results: number[] = [];
+    for (let i = 0; i < times; i++) {
+        const result = await measureRequest<T>(client, method, params);
+        results.push(result);
+    }
+    return results;
+}
+
+export enum Mode {
+    Concurrent,
+    Sequential,
+}
+
+export function logMemoryUsage() {
+    const totalMemory = process.memoryUsage().heapUsed;
+    console.log(
+        chalk.yellow(
+            `TOTAL MEMORY USAGE: ${(totalMemory / 1024 / 1024).toFixed(2)} MB`,
+        ),
+    );
+}
+
+export class Setup {
+    client: Connection;
+    rootUri: URI;
+    verbose: boolean;
+    name: string;
+    args: any;
+
+    constructor(
+        name: string,
+        client: Connection,
+        rootUri: URI,
+        verbose: boolean,
+        args: any,
+    ) {
+        this.client = client;
+        this.rootUri = rootUri;
+        this.verbose = verbose;
+        this.name = name;
+        this.args = args;
+    }
+
+    static async create(name: string, args: any) {
+        if (!args.root) {
+            console.error(
+                'USAGE: node out/benchmark.js --root <path_to_project> [--verbose]',
+            );
+            process.exit(1);
+        }
+        const root = resolve(args.root);
+        const verbose = !!args.verbose;
+        const rootUri = URI.file(root);
+
+        const [client, _server] = setupClientServer();
+
+        client.onNotification('textDocument/publishDiagnostics', (params) => {
+            if (verbose)
+                console.log(chalk.cyan(`RECEIVED DIAGNOSTICS: ${params.uri}`));
+        });
+
+        return new Setup(name, client, rootUri, verbose, args);
+    }
+
+    async sendNotification(
+        method: string,
+        params: object,
+        times?: number,
+    ): Promise<void> {
+        if (times) {
+            assert(times > 1, 'times must be greater than 0');
+            await Promise.all(
+                [...Array(times)].map((_, _i) =>
+                    this.client.sendNotification(method, params),
+                ),
+            );
+        } else {
+            await this.client.sendNotification(method, params);
+        }
+        if (this.verbose) {
+            console.log(
+                chalk.cyan(
+                    `SENT NOTIFICATION: ${method}${
+                        times ? ` ${times} times` : ''
+                    }`,
+                ),
+            );
+        }
+    }
+
+    async benchmark<T>(
+        method: string,
+        params: object,
+        times?: number,
+        mode: Mode = Mode.Concurrent,
+    ): Promise<void> {
+        if (times) {
+            let timings: number[];
+            if (mode === Mode.Concurrent) {
+                timings = await measureMultipleRequestConcurrent<T>(
+                    this.client,
+                    method,
+                    params,
+                    times,
+                );
+            } else {
+                timings = await measureMultipleRequestSequential<T>(
+                    this.client,
+                    method,
+                    params,
+                    times,
+                );
+            }
+            const total = timings.reduce((acc, t) => acc + t, 0);
+            const mean = total / times;
+            timings.sort((a, b) => a - b);
+            const mid = Math.floor(times / 2);
+            const median =
+                times % 2 !== 0
+                    ? timings[mid]
+                    : (timings[mid - 1] + timings[mid]) / 2;
+            const modeStr =
+                mode === Mode.Concurrent ? 'concurrently' : 'sequentially';
+            console.log(
+                chalk.yellow(
+                    `BENCHMARK: Request ${method} result (ran ${times} times ${modeStr}):`,
+                ),
+            );
+            console.table({
+                'Total (ms)': Number(total.toFixed(2)),
+                'Min (ms)': Number(timings[0].toFixed(2)),
+                'Max (ms)': Number(timings[timings.length - 1].toFixed(2)),
+                'Mean (ms)': Number(mean.toFixed(2)),
+                'Median (ms)': Number(median.toFixed(2)),
+            });
+        } else {
+            const result = await measureRequest(this.client, method, params);
+            console.log(
+                chalk.yellow(
+                    `BENCHMARK: Single ${method} request result: ${result.toFixed(
+                        2,
+                    )} ms`,
+                ),
+            );
+        }
+    }
+}
+
+export async function createBenchmark(
+    name: string,
+    scenario: (setup: Setup) => Promise<void>,
+) {
+    console.log(chalk.yellow(`Benchmark ${name} is running`));
+
+    const args = minimist(process.argv.slice(2));
+
+    const setup = await Setup.create(name, args);
+
+    await scenario(setup);
+}
+
+export const logInitializationTiming = (before: DOMHighResTimeStamp) => {
+    const after = performance.now();
+    console.log(
+        chalk.yellow(
+            `BENCHMARK: Initialization time: ${(after - before).toFixed(2)} ms`,
+        ),
+    );
+};
