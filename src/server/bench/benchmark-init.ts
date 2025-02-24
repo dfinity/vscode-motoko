90e581b1ee5b9e0c94167a09b0436c60d685779d
Savely Krendelhoff <alicecandyom@proton.me>
2025-02-24 14:44:44 +0700

    [VSC-6] Add benchmark init

---
+++ src/server/bench/benchmark-init.ts
@@ -0,0 +1,22 @@
+import { InitializeResult } from 'vscode-languageserver/node';
+import {
+    clientInitParams,
+    createBenchmark,
+    logInitializationTiming,
+    Setup,
+} from './helpers';
+
+createBenchmark('init', async (setup: Setup) => {
+    const before = performance.now();
+
+    setup.client.onNotification('custom/initialized', (_) =>
+        logInitializationTiming(before),
+    );
+
+    await setup.benchmark<InitializeResult>(
+        'initialize',
+        clientInitParams(setup.rootUri),
+    );
+
+    await setup.sendNotification('initialized', {});
+});
