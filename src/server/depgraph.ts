56b0f7334fd4f1c034777faa635df03becb00e3e
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2025-02-27 19:49:14 -0300

    [VSC-7] Support cache in the extension
    Problem: We'd like to speed up the extension by adding a cache for
    unchanged files.
    
    Solution: For the typed run, use the new `parseMotokoTypedLsp` function,
    store its cache, and give back this cache to the function on each typed
    run. For the untyped run, use the new `parseMotokoWithDeps`. For both
    cases, manage a build graph and use it to figure out when to remove
    things from the cache.

---
+++ src/server/depgraph.ts
@@ -0,0 +1,42 @@
+import { DepGraph as DG } from 'dependency-graph';
+
+export default class DepGraph {
+    private readonly _depGraph = new DG<string>({ circular: false });
+
+    clear() {
+        for (const node of this._depGraph.overallOrder()) {
+            this._depGraph.removeNode(node);
+        }
+    }
+
+    add(node: string) {
+        this._depGraph.addNode(node);
+    }
+
+    delete(node: string): boolean {
+        const hasNode = this._depGraph.hasNode(node);
+        this._depGraph.removeNode(node);
+        return hasNode;
+    }
+
+    addImmediateImports(from: string, immediateImports: string[]) {
+        for (const immediateImport of immediateImports) {
+            this._depGraph.addNode(immediateImport);
+            this._depGraph.addDependency(from, immediateImport);
+        }
+    }
+
+    transitiveDependencies(node: string): string[] {
+        return this._depGraph.dependenciesOf(node, false);
+    }
+
+    transitiveDependents(node: string): string[] {
+        return this._depGraph.dependentsOf(node, false);
+    }
+
+    removeImmediateDependencies(node: string) {
+        for (const file of this.transitiveDependencies(node)) {
+            this._depGraph.removeDependency(node, file);
+        }
+    }
+}
